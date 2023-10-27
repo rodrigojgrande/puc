4a538d5d17ab72b7a486fc2ef232a203cb7e7b45
Rodrigo Grande <rodrigojgrande@gmail.com>
2023-10-27 11:47:57 -0300

    Seminary - Class 05

---
+++ 01-periodo/seminario/aula-01/aula-05/final/functions.js
@@ -0,0 +1,41 @@
+function login(){
+    document.getElementById('login').className = "display-3";
+    document.getElementById('usuario').className = "h5";
+    document.getElementById('senha').className = "h5";
+    document.getElementById('entrar').className = "h3";
+    document.getElementById('limpar').className = "h3";
+}
+
+function curriculo(){
+    document.getElementById('curriculo').className = "display-3";
+    document.getElementById('dadospessoais').className = "h3";
+    document.getElementById('experiencias').className = "h3";
+    document.getElementById('formacao').className = "h3";
+}
+
+function dadospessoais(){
+    document.getElementById('dados').className = "display-3";
+    document.getElementById('nomecompleto').className = "h3";
+    document.getElementById('email').className = "h3";
+    document.getElementById('telefone').className = "h3";
+    document.getElementById('entrar').className = "h3";
+    document.getElementById('limpar').className = "h3";
+}
+
+function experiencias() {
+    document.getElementById('experiencias').className = "display-3";
+    document.getElementById('instituicao').className = "h3";
+    document.getElementById('atividade').className = "h3";
+    document.getElementById('data').className = "h3";
+    document.getElementById('entrar').className = "h3";
+    document.getElementById('limpar').className = "h3";
+}
+
+function formacao() {
+    document.getElementById('formacao').className = "display-3";
+    document.getElementById('instituicao').className = "h3";
+    document.getElementById('curso').className = "h3";
+    document.getElementById('periodo').className = "h3";
+    document.getElementById('entrar').className = "h3";
+    document.getElementById('limpar').className = "h3";
+}
---
+++ 01-periodo/seminario/aula-01/aula-05/final/login.js
@@ -0,0 +1,7 @@
+function login(){
+    document.getElementById('login').className = "display-3";
+    document.getElementById('usuario').className = "h5";
+    document.getElementById('senha').className = "h5";
+    document.getElementById('entrar').className = "h3";
+    document.getElementById('limpar').className = "h3";
+}
