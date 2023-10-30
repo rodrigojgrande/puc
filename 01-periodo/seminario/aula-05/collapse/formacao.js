aaddef0888af1f893dde4e1efb17d9d5c3a2971d
Rodrigo Grande <rodrigojgrande@gmail.com>
2023-10-30 18:37:27 -0300

    Seminary - Finished (Reorganizing)

---
+++ 01-periodo/seminario/aula-05/collapse/formacao.js
@@ -0,0 +1,8 @@
+function formacao() {
+    document.getElementById('formacao').className = "display-3";
+    document.getElementById('instituicao').className = "h3";
+    document.getElementById('curso').className = "h3";
+    document.getElementById('periodo').className = "h3";
+    document.getElementById('entrar').className = "h3";
+    document.getElementById('limpar').className = "h3";
+}
---
+++ 01-periodo/seminario/seminario-final/js/login.js
@@ -0,0 +1,7 @@
+function login(){
+    document.getElementById('llogin').className = "display-3";
+    document.getElementById('lusuario').className = "h5";
+    document.getElementById('lsenha').className = "h5";
+    document.getElementById('lentrar').className = "h3";
+    document.getElementById('llimpar').className = "h3";
+}
