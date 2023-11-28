7e93b890fb8537016da15df47958f690cf52d889
Rodrigo Grande <rodrigojgrande@gmail.com>
2023-11-28 10:14:34 -0300

    Final Project - LPHS - 3° Reunion - PIR Sensor

--- 01-periodo/lphs/projetos/10-mascote/mascote.ino
+++ 01-periodo/lphs/projetos/10-mascote/mascote.ino
@@ -1,3 +1,30 @@
+/*
+DOCUMENTAÇÃO
+Portas Utilizando:
+02 => LCD
+03 => LCD
+04 => LCD
+05 => LCD
+08 => PIR
+09 =>  SERVO
+10 => SERVO
+11 => LCD
+12 => LCD
+
+Portas Livres: 06 - 07 - 13
+
+Fios:
+Vermelho: 5V
+Marrom: GND
+Laranja: Portas
+Roxo: Potenciometro
+Azul: Servo Right Arm
+Verde: Servo Left Arm 
+Amarelo: Pir Sensor
+
+
+*/
+
 // Servo Motor
 #include <Servo.h>
 
@@ -5,8 +32,8 @@
 #include <LiquidCrystal.h>
 
 // <SERVO>
-#define SERVOLEFTARM 9 // Porta Digital 2
-#define SERVORIGHTARM 10 // Porta Digital 3
+#define SERVOLEFTARM 9 // Porta Digital 9
+#define SERVORIGHTARM 10 // Porta Digital 10
 
 Servo leftArm; // Variável Servo
 Servo rightArm; // Variável Servo
@@ -15,8 +42,14 @@ int positionLeftArm; // Posição Servo
 int positionRightArm; // Posição Servo
 // </SERVO>
 
-// Inicializa a biblioteca com os pinos da interface
+// LCD
+// Inicializa a biblioteca com os pinos da interface LCD
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// PIR
+int pirSensor = 8; // Pino Sensor de Presença
+int active = 1;
+int inactive = 0;
 
 void setup () {
   // <SERVO>
@@ -27,53 +60,65 @@ void setup () {
   rightArm.write(0); // Inicia motor posição zero
   // </SERVO>
 
+  // <PIR>
+  //pinMode(pinoLed, OUTPUT); // Define o pino como saída
+  pinMode(pirSensor, INPUT); //Define o pino como entrada
+  // </PIR>
+
   // <LCD>
   // Configura as linhas e colunas do LCD
   lcd.begin(16, 2);
   // Escreve na tela do LCD
-
-  // PARADO
-  lcd.setCursor(0,0);
-  lcd.print("    0      0   ");
-  lcd.setCursor(0,1);
-  lcd.print("      ----     ");
-  
-  // DANÇANDO
-  // lcd.setCursor(0,0);
-  // lcd.print("    >      <   ");
-  // lcd.setCursor(0,1);
-  // lcd.print("      ----     ");
-  //</LCD>
 }
 
 void loop(){
-  // <SERVO>
-  // <LEFT ARM>
-  for(positionLeftArm = 0; positionLeftArm < 90; positionLeftArm++) {
-    leftArm.write(positionLeftArm);
-  }
-  // </LEFT ARM>
+  // <PIR>
+  if(digitalRead(pirSensor) == HIGH){ //Se a leitura for igual a HIGH
+    // DANÇANDO
+    lcd.setCursor(0,0);
+    lcd.print("    >      <   ");
+    lcd.setCursor(0,1);
+    lcd.print("      ----     ");
+    //</LCD>
 
-  // <RIGHT ARM>
-  for(positionRightArm = 0; positionRightArm < 90; positionRightArm++) {
-    rightArm.write(positionRightArm);
-  }
-  // </RIGHT ARM>
+    // <SERVO>
+    // <LEFT ARM>
+    for(positionLeftArm = 0; positionLeftArm < 90; positionLeftArm++) {
+      leftArm.write(positionLeftArm);
+    }
+    // </LEFT ARM>
 
-  delay(500);
+    // <RIGHT ARM>
+    for(positionRightArm = 0; positionRightArm < 90; positionRightArm++) {
+      rightArm.write(positionRightArm);
+    }
+    // </RIGHT ARM>
 
-  // <LEFT ARM>
-  for(positionLeftArm = 90; positionLeftArm >= 0; positionLeftArm--) {
-    leftArm.write(positionLeftArm);
-  }
-  // </LEFT ARM>
+    delay(500);
 
-  // <RIGHT ARM>
-  for(positionRightArm = 90; positionRightArm >= 0; positionRightArm--) {
-    rightArm.write(positionRightArm);
+    // <LEFT ARM>
+    for(positionLeftArm = 90; positionLeftArm >= 0; positionLeftArm--) {
+      leftArm.write(positionLeftArm);
+    }
+    // </LEFT ARM>
+
+    // <RIGHT ARM>
+    for(positionRightArm = 90; positionRightArm >= 0; positionRightArm--) {
+      rightArm.write(positionRightArm);
+    }
+    // </RIGHT ARM>
+    // </SERVO>
+    Serial.println(active);
+  }else{ 
+    // PARADO
+    lcd.setCursor(0,0);
+    lcd.print("    0      0   ");
+    lcd.setCursor(0,1);
+    lcd.print("      ----     ");
+    // PARADO
+
+    Serial.println(inactive);
   }
-  // </RIGHT ARM>
-  // </SERVO>
+  // </PIR>
   delay(1000);
-  
 }
