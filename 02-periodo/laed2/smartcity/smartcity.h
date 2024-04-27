800bfd8f3c56b0cc8ba652d9a273e7160b5fbcea
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-04-27 16:42:49 -0300

    LAEDII - Smartcity

---
+++ 02-periodo/laed2/smartcity/smartcity.h
@@ -0,0 +1,29 @@
+
+#ifndef SMART_CITY_H
+#define SMART_CITY_H
+
+typedef struct
+{
+    int codigo;
+    char nome[30];
+    float comprimento;
+    int tipo_piso;
+} via;
+
+typedef struct
+{
+    via *vias;
+    int capacidade;
+    int tamanho;
+} smart_city;
+
+// Funcoes
+smart_city *criaCidade(smart_city *cidade, int capacidade);
+void redimensionaCapacidade(smart_city *cidade);
+void adicionaVia(smart_city *cidade, via* novaVia);
+void removeVia(smart_city *cidade, via* viaRemover);
+void exibeVias(smart_city cidade);
+void liberaMemoria(smart_city *cidade);
+
+#endif
+
