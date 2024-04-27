f5da13c6bf0b6dfd389cfaeaee506604bb57b9e1
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-04-27 16:41:43 -0300

    LAEDII - 10/04/2024

---
+++ 02-periodo/laed2/20240410/tarefa-04.c
@@ -0,0 +1,4 @@
+/*
+4 - Implemente a função void adicionaVetor(VetorDinamico *vetor, int elements) que adiciona um 
+elemento ao vetor. A função deverá redimencionar o vetor se o tamanho alcançar a capacidade
+*/
---
+++ 02-periodo/laed2/20240410/tarefa-06.c
@@ -0,0 +1,4 @@
+/*
+6 - Implemente a função void removeElemento (vetorDinamico *vetor, int, indice)
+que remova um elemento em um índice especificado e ajusta o tamanho do vetor se necessário
+*/
