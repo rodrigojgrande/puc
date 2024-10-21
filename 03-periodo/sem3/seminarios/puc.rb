a78db2facc0e33c530ace1f239ab8126d4760ab2
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-10-21 11:23:18 -0300

    SEMIII - Hello World in Ruby

---
+++ 03-periodo/sem3/seminarios/puc.rb
@@ -0,0 +1,11 @@
+puts "Digite o valor de X" # Escreve na tela
+
+x = gets.chomp.to_i # Captura uma informação
+
+puts "Digite o valor de Y" # Escreve na tela
+
+z = gets.chomp.to_i # Captura uma informação
+
+teste = x+z
+
+puts "O valor de X é #{teste}"
