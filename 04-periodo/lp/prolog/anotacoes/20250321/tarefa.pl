b40b91847de055e6cca13415e2127c2547da3a98
Rodrigo Grande <rodrigojgrande@gmail.com>
2025-05-06 22:58:13 -0300

    LP - 21/03/2025

---
+++ 04-periodo/lp/prolog/anotacoes/20250321/tarefa.pl
@@ -0,0 +1,17 @@
+progenitor(joao, maria).
+progenitor(ana, maria).
+progenitor(geraldo, zezinho). 
+progenitor(bianca, zezinho). 
+progenitor(joao, dudu). 
+progenitor(ana, dudu). 
+
+mulher(ana).
+mulher(bianca).
+mulher(maria).
+
+homem(joao).
+homem(geraldo).
+homem(dudu).
+homem(zezinho).
+
+% Criar regra de quem é mãe e quem é pai
